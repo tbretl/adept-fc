40845b5e172b65b77d81903ed656cd6bb7dbe9d2
Aaron Perry <atperry2@illinois.edu>
2019-04-01 11:06:01 -0500

    added initial message headers

---
+++ message_types/generate_message_headers.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+zcm-gen -cpp sensor_accel_t.zcm 
+zcm-gen -cpp sensor_airdata_t.zcm 
+zcm-gen -cpp sensor_euler_t.zcm 
+zcm-gen -cpp sensor_gyro_t.zcm
+zcm-gen -cpp sensor_mag_t.zcm 
+zcm-gen -cpp sensor_baro_t.zcm 
+zcm-gen -cpp sensor_gps_t.zcm 
