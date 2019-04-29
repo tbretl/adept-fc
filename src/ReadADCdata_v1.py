660ce3785dfa29c19d897ae6b625e1c25b3e615f
Yukti Kathuria <ykathur2@illinois.edu>
2019-04-29 09:18:02 -0500

    Update ReadADCdata_v1.py
    Currently able to output values from the SPI interface but not sure if those values correspond to the ADC value

--- src/ReadADCdata_v1.py
+++ src/ReadADCdata_v1.py
@@ -6,8 +6,8 @@ import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 spi=spidev.SpiDev()
-spi.open(0,0)
-spi.max_speed_hz=24000000   #16bits=1data, 1.5MSPS needed
+spi.open(0,1)
+spi.max_speed_hz=4000000   #16bits=1data, 1.5MSPS needed
 
 SCLK=23
 CS=26
@@ -15,24 +15,23 @@ MISO=21
 GPIO.setup(SCLK,GPIO.OUT)
 GPIO.setup(CS,GPIO.OUT)
 GPIO.setup(MISO,GPIO.IN)
-GPIO.output(CS,GPIO.HIGH)   #initialize CS
+GPIO.output(CS,GPIO.LOW)   #initialize CS
 
 def readADC(clkPin,csPin,misoPin):
-   buff=spi.readbytes(2)
-   bData=bin(buff[0])[2:].zfill(8)[2:]+bin(buff[1])[2:].zfill(8)
-   bData_rev=bData[::-1]
+   buff=spi.readbytes(4)
    data=0
-   for bit in bData_rev:
-         data=(data<<1)|int(bit)
-return data
+   for bit in buff:
+         data=(data<<8)|int(bit)
+         data |= 0xFF
+   return data
 if __name__=='__main__':
    try:
          while True:
-                GPIO.output(CS,GPIO.HIGH)  #CS active
+                GPIO.output(CS,GPIO.LOW)  #CS active
                 val=readADC(SCLK,CS,MISO)
                 volt=val*3.3/(2**14)
-                print 'ADC value', str(volt)
-                GPIO.output(CS,GPIO.LOW)   #CS inactive
+                print('ADC value', str(volt))
+                GPIO.output(CS,GPIO.HIGH)   #CS inactive
    except KeyboardInterrupt:
          GPIO.cleanup()
          spi.close()
