5d3b578957ba7efc003420ef78a4281989a01f9c
Yukti Kathuria <ykathur2@illinois.edu>
2019-05-01 04:49:27 -0500

    Update ReadADCdata_v1.py
    Added sleep and wakeup functions, removed the setup of GPIO pins as that is unnecessary, abstracted the configuration bit.

--- src/ReadADCdata_v1.py
+++ src/ReadADCdata_v1.py
@@ -6,34 +6,75 @@ import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 spi=spidev.SpiDev()
-spi.open(0,1)
-spi.max_speed_hz=4000000   #16bits=1data, 1.5MSPS needed
 
 SCLK=23
 CS=26
 MISO=21
-GPIO.setup(SCLK,GPIO.OUT)
-GPIO.setup(CS,GPIO.OUT)
-GPIO.setup(MISO,GPIO.IN)
-GPIO.output(CS,GPIO.LOW)   #initialize CS
 
-def readADC(clkPin,csPin,misoPin):
-   buff=spi.readbytes(4)
-   data=0
-   for bit in buff:
-         data=(data<<8)|int(bit)
-         data |= 0xFF
-   return data
+spi.open(0,1)
+spi.max_speed_hz = 4000000
+spi.mode = 0
+buff = 0
+LTC_CONFIG_SLEEP = 1
+LTC_CONFIG_UNI = 2
+LTC_CONFIG_COM = 3
+LTC_CONFIG_S0 = 4
+LTC_CONFIG_S1 = 5
+LTC_CONFIG_ODD = 6
+LTC_CONFIG_SINGLE_END = 7
+
+#LTC_CHAN_SINGLE_0P = bin(LTC_CONFIG_SINGLE_END)
+#LTC_CHAN_SINGLE_1P = int(bin(LTC_CONFIG_SINGLE_END)) | int(bin(LTC_CONFIG_ODD))
+#LTC_CHAN_SINGLE_2P = bin(LTC_CONFIG_SINGLE_END) | bin(LTC_CONFIG_S0)
+#LTC_CHAN_SINGLE_3P = bin(LTC_CONFIG_SINGLE_END) | bin(LTC_CONFIG_S0) | bin(LTC_CONFIG_ODD)
+#LTC_CHAN_SINGLE_4P = bin(LTC_CONFIG_SINGLE_END) | bin(LTC_CONFIG_S1)
+#LTC_CHAN_SINGLE_5P = bin(LTC_CONFIG_SINGLE_END) | bin(LTC_CONFIG_S1) | bin(LTC_CONFIG_ODD)
+#LTC_CHAN_SINGLE_6P = bin(LTC_CONFIG_SINGLE_END) | bin(LTC_CONFIG_S1) | bin(LTC_CONFIG_S0) 
+#LTC_CHAN_SINGLE_7P = bin(LTC_CONFIG_SINGLE_END) | bin(LTC_CONFIG_S1) | bin(LTC_CONFIG_S0) | bin(LTC_CONFIG_ODD)
+currentLTCconfig = None
+
+
+def Sleep():
+	currentLTCconfig |= 0x01
+	spi.xfer(currentLTCconfig)
+	spi.xfer(0)
+
+	
+
+
+
+def wake():
+	wasSleep = currentLTCconfig & bin(LTC_CONFIG_SLEEP)
+	currentLTCconfig &= ~bin(LTC_CONFIG_SLEEP)
+	spi.xfer(currentLTCconfig)
+	spi.xfer([0])	
+	if(wasSleep):
+		delay(70)
+	
+	
+	
+
+def readADC(clkPin,csPin,misoPin,currentLTCconfig):
+
+	buff=spi.xfer2(currentLTCconfig)
+	
+	#newbuff = spi.xfer([0])
+	#for y in buff:
+	#	buff[y]<<8
+	#	buff[y] |= 0xFF #& (newbuff[y]>> 4) 
+	return buff
+
+
+
+	
 if __name__=='__main__':
-   try:
-         while True:
-                GPIO.output(CS,GPIO.LOW)  #CS active
-                val=readADC(SCLK,CS,MISO)
-                volt=val*3.3/(2**14)
-                print('ADC value', str(volt))
-                GPIO.output(CS,GPIO.HIGH)   #CS inactive
-   except KeyboardInterrupt:
-         GPIO.cleanup()
-         spi.close()
-         sys.exit(0)
-#########################
+	try:
+		while True:
+			currentLTCconfig = [0x80]
+			val=readADC(SCLK,CS,MISO,currentLTCconfig)
+			volt=val
+			print('ADC value', str(volt))
+	except KeyboardInterrupt:
+			GPIO.cleanup()
+			spi.close()
+			sys.exit(0)
