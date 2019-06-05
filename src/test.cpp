22d2cd02dc8a543b265590c8b6ad2d16497df0d6
Katherine O'Connell <ko31@illinois.edu>
2019-06-05 18:00:24 -0500

    test file-Fibonacci

--- src/test.cpp
+++ src/test.cpp
@@ -2,7 +2,25 @@
 
 
 int main() {
-    std::cout << "hello world" << std::endl;
+
+    int n, c, first = 0, second = 1, next;
+
+    std::cout << "Enter the number of terms in the Fibonacci series you want" << std::endl;
+
+    std::cin >> n;
+
+    for ( c = 0 ; c < n ; c++ )
+    {
+        if ( c <= 1 )
+            next = c;
+        else
+        {
+            next = first + second;
+            first = second;
+            second = next;
+        }
+        cout << next << endl;
+    }
 
     return 0;
 }
