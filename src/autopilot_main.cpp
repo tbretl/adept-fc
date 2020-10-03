38daed40e134db106bb1a9acdaf307f9c155a6e3
Grayson Schaer <gschaer2@illinois.edu>
2020-10-03 13:42:21 -0500

    Update autopilot_main.cpp
    Corrected typo in eval poly fnc. Corrected velocity calculation. Changed variables from float to double. Corrected typo in bad state rejection.

--- src/autopilot_main.cpp
+++ src/autopilot_main.cpp
@@ -59,15 +59,14 @@ double get_gps_time(Handler* adchandle)
     return  adc_gps + (rpi_time - adc_time)/1000000.0;
 }
 
-float evaluate_poly(float coeffs[], size_t size, float X, float Y) {
-    int order = int(size);
+double evaluate_poly(double coeffs[], int size, double X, double Y) {
     int curr_x = 0;
     int curr_y = 0;
     int x_order = 0;
     int y_order = 0;
-    float evaluation = 0.0;
+    double evaluation = 0.0;
 
-    for (int i = 0; i < order; i++) {
+    for (int i = 0; i < size; i++) {
         evaluation += coeffs[i] * pow(X, curr_x) * pow(Y, curr_y);
         if (curr_x == 0) {
             curr_x = x_order + 1;
@@ -90,138 +89,119 @@ float evaluate_poly(float coeffs[], size_t size, float X, float Y) {
 
 int main(int argc, char *argv[])
 {
-    //_________________________________START_PASTE_HERE_________________________________//
-   std::cout << "Starting autopilot..." << std::endl;
-   std::cout << "Creating variables..." << std::endl;
 
    // Conversion constants for the adc inputs (polynomial coefficients from c0*x^0 to cn*x^n)
-   float P1_c[2] = { -0.19188, 4.8261e-06 }; // To dPSI
-   float P2_c[2] = { -0.19142, 4.8269e-06 }; // To dPSI
-   float P3_c[2] = { -0.19211, 4.8141e-06 }; // To dPSI
-   float P4_c[2] = { -0.19283, 4.8152e-06 }; // To dPSI
-   float P5_c[2] = { -0.19231, 4.8028e-06 }; // To dPSI
-   float r_ail_c[8] = { -4588.2213, 1.2474, -0.00014428, 9.1555e-09, -3.4398e-13, 7.6526e-18, -9.3369e-23, 4.8223e-28 }; // To degrees
-   float l_ail_c[5] = { 4.0069, 0.004463, -4.079e-07, 1.3728e-11, -1.7482e-16 }; // To degrees
-   float r_ele_c[4] = { 34.3489, -0.001876, 6.0969e-08, -1.836e-12 }; // To degrees
-   float l_ele_c[5] = { -68.4075, 0.019632, -1.4238e-06, 3.9725e-11, -4.1426e-16 }; // To degrees
-   float rud_c[5] = { 1212.7197, -0.17556, 9.2592e-06, -2.1584e-10, 1.8988e-15 }; // To degrees
+   double P1_c[2] = { -0.19188, 4.8261e-06 }; // To dPSI
+   double P2_c[2] = { -0.19142, 4.8269e-06 }; // To dPSI
+   double P3_c[2] = { -0.19211, 4.8141e-06 }; // To dPSI
+   double P4_c[2] = { -0.19283, 4.8152e-06 }; // To dPSI
+   double P5_c[2] = { -0.19231, 4.8028e-06 }; // To dPSI
 
    // Conversion constants for the converted adc data
-   float alpha_c[15] = { 0.14417, -14.0401, -0.48222, -0.82991, -0.39334, -0.065129, 0.29331, 0.10864, 0.57212, 0.12463, 0.022992, 0.029209, 0.089836, 0.057237, 0.016901 };
-   float beta_c[15] = { -0.045676, 0.090171, 13.8048, 0.002027, 0.94476, 0.29254, 0.12192, -0.66955, -0.020875, -0.33687, 0.023934, -0.10505, -0.019041, -0.054968, -0.018293 };
-   float cps_c[21] = { 0.59235, -0.0032055, -0.0045759, -0.098045, 0.010147, -0.0977, -0.015851, -0.0024914, -0.022443, -0.0037574, -0.0042317, -0.0039405, 0.01926, -0.0014971, -0.0014967, -0.0007027, -0.0010546, 0.0041895, 6.6051e-05, 0.00048148, -0.00022731 };
-   float cpt_c[15] = { -0.21483, 0.036139, 0.0037369, -0.12377, -0.034201, -0.11844, 0.0022027, 0.0040131, 0.0047189, 0.0026645, 0.00010707, 0.0023433, 0.0079094, 0.0034925, -0.001166 };
+   double alpha_c[15] = { 0.14417, -14.0401, -0.48222, -0.82991, -0.39334, -0.065129, 0.29331, 0.10864, 0.57212, 0.12463, 0.022992, 0.029209, 0.089836, 0.057237, 0.016901 };
+   double beta_c[15] = { -0.045676, 0.090171, 13.8048, 0.002027, 0.94476, 0.29254, 0.12192, -0.66955, -0.020875, -0.33687, 0.023934, -0.10505, -0.019041, -0.054968, -0.018293 };
+   double cps_c[21] = { 0.59235, -0.0032055, -0.0045759, -0.098045, 0.010147, -0.0977, -0.015851, -0.0024914, -0.022443, -0.0037574, -0.0042317, -0.0039405, 0.01926, -0.0014971, -0.0014967, -0.0007027, -0.0010546, 0.0041895, 6.6051e-05, 0.00048148, -0.00022731 };
+   double cpt_c[15] = { -0.21483, 0.036139, 0.0037369, -0.12377, -0.034201, -0.11844, 0.0022027, 0.0040131, 0.0047189, 0.0026645, 0.00010707, 0.0023433, 0.0079094, 0.0034925, -0.001166 };
 
    // Controller constants
-   float k_lon[2][4] = {  {0, 0, 0, 0},  {0, 0, 0, 0} }; // Longitudinal controller gains of form u_lon = -k_lon * x_lon
-   float k_lat[2][5] = {  {0, 0.14855, 0, 0.4, 0},  {0, 0, 0, 0, 0} }; // Lateral controller gains of form u_lat = -k_lat * x_lat
+   double k_lon[2][4] = { {0, 0, 0, 0},  {0, 0, 0, 0} }; // Longitudinal controller gains of form u_lon = -k_lon * x_lon
+   double k_lat[2][5] = { {0, 0.14855, 0, 0.4, 0},  {0, 0, 0, 0, 0} }; // Lateral controller gains of form u_lat = -k_lat * x_lat
 
    // Trim conditions
-   float V_0 = 30.5755; // m/s
-   float alpha_0 = 0.0178; // rad
-   float q_0 = 0; // rad/s
-   float theta_0 = 0; // rad
-   float beta_0 = 0; // rad
-   float p_0 = 0; // rad/s
-   float r_0 = 0; // rad/s
-   float phi_0 = 0; // rad
-   float psi_0 = 0; // rad
+   double V_0 = 30.5755; // m/s
+   double alpha_0 = 0.0178; // rad
+   double q_0 = 0; // rad/s
+   double theta_0 = 0; // rad
+   double beta_0 = 0; // rad
+   double p_0 = 0; // rad/s
+   double r_0 = 0; // rad/s
+   double phi_0 = 0; // rad
+   double psi_0 = 0; // rad
+   double rho = 1.1651; // in kg/m^3
 
    // State limits
-   float V_min = 0; // Minimum acceptable value. Any values lower are considered improper readings.
-   float V_max = 50; // Maximum acceptable value. Any values higher are considered improper readings.
-   float alpha_min = -0.6108; // Minimum acceptable value. Any values lower are considered improper readings.
-   float alpha_max = 0.6108; // Maximum acceptable value. Any values higher are considered improper readings.
-   float q_min = -0.6108; // Minimum acceptable value. Any values lower are considered improper readings.
-   float q_max = 0.6108; // Maximum acceptable value. Any values higher are considered improper readings.
-   float theta_min = -1.0472; // Minimum acceptable value. Any values lower are considered improper readings.
-   float theta_max = 1.0472; // Maximum acceptable value. Any values higher are considered improper readings.
-   float beta_min = -0.6108; // Minimum acceptable value. Any values lower are considered improper readings.
-   float beta_max = 0.6108; // Maximum acceptable value. Any values higher are considered improper readings.
-   float p_min = -1.0471; // Minimum acceptable value. Any values lower are considered improper readings.
-   float p_max = 1.0471; // Maximum acceptable value. Any values higher are considered improper readings.
-   float r_min = -0.5236; // Minimum acceptable value. Any values lower are considered improper readings.
-   float r_max = 0.5236; // Maximum acceptable value. Any values higher are considered improper readings.
-   float phi_min = -1.0472; // Minimum acceptable value. Any values lower are considered improper readings.
-   float phi_max = 1.0472; // Maximum acceptable value. Any values higher are considered improper readings.
+   double V_min = 0.0; // Minimum acceptable value. Any values lower are considered improper readings.
+   double V_max = 60.0; // Maximum acceptable value. Any values higher are considered improper readings.
+   double alpha_min = -0.6108; // Minimum acceptable value. Any values lower are considered improper readings.
+   double alpha_max = 0.6108; // Maximum acceptable value. Any values higher are considered improper readings.
+   double q_min = -0.6108; // Minimum acceptable value. Any values lower are considered improper readings.
+   double q_max = 0.6108; // Maximum acceptable value. Any values higher are considered improper readings.
+   double theta_min = -1.0472; // Minimum acceptable value. Any values lower are considered improper readings.
+   double theta_max = 1.0472; // Maximum acceptable value. Any values higher are considered improper readings.
+   double beta_min = -0.6108; // Minimum acceptable value. Any values lower are considered improper readings.
+   double beta_max = 0.6108; // Maximum acceptable value. Any values higher are considered improper readings.
+   double p_min = -1.0471; // Minimum acceptable value. Any values lower are considered improper readings.
+   double p_max = 1.0471; // Maximum acceptable value. Any values higher are considered improper readings.
+   double r_min = -0.5236; // Minimum acceptable value. Any values lower are considered improper readings.
+   double r_max = 0.5236; // Maximum acceptable value. Any values higher are considered improper readings.
+   double phi_min = -1.0472; // Minimum acceptable value. Any values lower are considered improper readings.
+   double phi_max = 1.0472; // Maximum acceptable value. Any values higher are considered improper readings.
 
    // Input limits
-   float de_min = -0.7853; // Minimum acceptable value. Any values lower will be rounded to min.
-   float de_max = 0.7853; // Maximum acceptable value. Any values higher will be rounded to max.
-   float da_min = -0.7853; // Minimum acceptable value. Any values lower will be rounded to min.
-   float da_max = 0.7853; // Maximum acceptable value. Any values higher will be rounded to max.
-   float dr_min = -0.7853; // Minimum acceptable value. Any values lower will be rounded to min.
-   float dr_max = 0.7853; // Maximum acceptable value. Any values higher will be rounded to max.
-   float dt_min = 0; // Minimum acceptable value. Any values lower will be rounded to min.
-   float dt_max = 1; // Maximum acceptable value. Any values higher will be rounded to max.
+   double de_min = -0.7853; // Minimum acceptable value. Any values lower will be rounded to min.
+   double de_max = 0.7853; // Maximum acceptable value. Any values higher will be rounded to max.
+   double da_min = -0.7853; // Minimum acceptable value. Any values lower will be rounded to min.
+   double da_max = 0.7853; // Maximum acceptable value. Any values higher will be rounded to max.
+   double dr_min = -0.7853; // Minimum acceptable value. Any values lower will be rounded to min.
+   double dr_max = 0.7853; // Maximum acceptable value. Any values higher will be rounded to max.
+   double dt_min = 0; // Minimum acceptable value. Any values lower will be rounded to min.
+   double dt_max = 1; // Maximum acceptable value. Any values higher will be rounded to max.
+
 
    // Previous states
-   float V_prev = 0.0; // m/s
-   float alpha_prev = 0.0; // rad
-   float q_prev = 0.0; // rad/s
-   float theta_prev = 0.0; // rad
-   float beta_prev = 0.0; // rad
-   float p_prev = 0.0; // rad/s
-   float r_prev = 0.0; // rad/s
-   float phi_prev = 0.0; // rad
+   double V_prev = 0.0; // m/s
+   double alpha_prev = 0.0; // rad
+   double q_prev = 0.0; // rad/s
+   double theta_prev = 0.0; // rad
+   double beta_prev = 0.0; // rad
+   double p_prev = 0.0; // rad/s
+   double r_prev = 0.0; // rad/s
+   double phi_prev = 0.0; // rad
 
    // Input trim values
-   float de_0 = -0.0832; // rad
-   float da_0 = 0; // rad
-   float dr_0 = 0; // rad
-   float dt_0_0 = 0.49188; //percent throttle [0.0, 1.0]
-   float dt_1_0 = 0.49188; //percent throttle [0.0, 1.0]
-   float dt_2_0 = 0.49188; //percent throttle [0.0, 1.0]
-   float dt_3_0 = 0.49188; //percent throttle [0.0, 1.0]
-   float dt_4_0 = 0.49188; //percent throttle [0.0, 1.0]
-   float dt_5_0 = 0.49188; //percent throttle [0.0, 1.0]
-   float dt_6_0 = 0.49188; //percent throttle [0.0, 1.0]
-   float dt_7_0 = 0.49188; //percent throttle [0.0, 1.0]
+   double de_0 = -0.0832; // rad
+   double da_0 = 0; // rad
+   double dr_0 = 0; // rad
+   double dt_0_0 = 0.49188; //percent throttle [0.0, 1.0]
+   double dt_1_0 = 0.49188; //percent throttle [0.0, 1.0]
+   double dt_2_0 = 0.49188; //percent throttle [0.0, 1.0]
+   double dt_3_0 = 0.49188; //percent throttle [0.0, 1.0]
+   double dt_4_0 = 0.49188; //percent throttle [0.0, 1.0]
+   double dt_5_0 = 0.49188; //percent throttle [0.0, 1.0]
+   double dt_6_0 = 0.49188; //percent throttle [0.0, 1.0]
+   double dt_7_0 = 0.49188; //percent throttle [0.0, 1.0]
 
    // Declare all other values used
-   float P1;
-   float P2;
-   float P3;
-   float P4;
-   float P5;
-   float r_ail;
-   float l_ail;
-   float r_ele;
-   float l_ele;
-   float rud;
-   float P_avg;
-   float C_alpha;
-   float C_beta;
-   float alpha;
-   float beta;
-   float cps;
-   float cpt;
-   float Pt;
-   float Ps;
-   float rho;
-   float V;
-   float yaw;
-   float pitch;
-   float roll;
-   float wx;
-   float wy;
-   float wz;
-   float q;
-   float theta;
-   float p;
-   float r;
-   float phi;
-   float de_percent;
-   float da_percent;
-   float dr_percent;
-   float lon_states[4];
-   float lat_states[5];
-   float u_lon_0;
-   float u_lon_1;
-   float u_lat_0;
-   float u_lat_1;
-
-   std::cout << "Initializing ZCM..." << std::endl;
+   double P1;
+   double P2;
+   double P3;
+   double P4;
+   double P5;
+   double P_avg;
+   double C_alpha;
+   double C_beta;
+   double alpha;
+   double beta;
+   double cps;
+   double cpt;
+   double Pt;
+   double Ps;
+   double V;
+   double yaw;
+   double pitch;
+   double roll;
+   double wx;
+   double wy;
+   double wz;
+   double de_percent;
+   double da_percent;
+   double dr_percent;
+   double lon_states[4];
+   double lat_states[5];
+   double u_lon_0;
+   double u_lon_1;
+   double u_lat_0;
+   double u_lat_1;
 
    //initialize zcm
     zcm::ZCM zcm {"ipc"};
@@ -252,42 +232,33 @@ int main(int argc, char *argv[])
        //publish the status of this module
        zcm.publish("STATUS4",&module_stat);
 
-       // Raw data from adc
-       P1 = (float)handlerObject.adc.data[0]; // uCH0 --> uncalibrated P1 for 5 hole probe
-       P2 = (float)handlerObject.adc.data[1]; // uCH1 --> uncalibrated P2 for 5 hole probe
-       P3 = (float)handlerObject.adc.data[2]; // uCH2 --> uncalibrated P3 for 5 hole probe
-       P4 = (float)handlerObject.adc.data[3]; // uCH3 --> uncalibrated P4 for 5 hole probe
-       P5 = (float)handlerObject.adc.data[4]; // uCH4 --> uncalibrated P5 for 5 hole probe
-       r_ail = (float)handlerObject.adc.data[8]; // dCH0 --> uncalibrated right aileron position
-       l_ail = (float)handlerObject.adc.data[9]; // dCH1 --> uncalibrated left aileron position
-       r_ele = (float)handlerObject.adc.data[10]; // dCH2 --> uncalibrated right elevator position
-       l_ele = (float)handlerObject.adc.data[11]; // dCH3 --> uncalibrated left elevator position
-       rud = (float)handlerObject.adc.data[12]; // dCH4 --> uncalibrated rudder position
+       // Conversion of raw adc data to pressure data
+       P1 = P1_c[0]  + P1_c[1] * (float)handlerObject.adc.data[0]; // in dPSI
+       P2 = P2_c[0]  + P2_c[1] * (float)handlerObject.adc.data[1]; // in dPSI
+       P3 = P3_c[0]  + P3_c[1] * (float)handlerObject.adc.data[2]; // in dPSI
+       P4 = P4_c[0]  + P4_c[1] * (float)handlerObject.adc.data[3]; // in dPSI
+       P5 = P5_c[0]  + P5_c[1] * (float)handlerObject.adc.data[4]; // in dPSI
 
-       // Conversion of raw adc data
-       P1 = P1_c[0] * pow(P1, 0) + P1_c[1] * pow(P1, 1); // in dPSI
-       P2 = P2_c[0] * pow(P2, 0) + P2_c[1] * pow(P2, 1); // in dPSI
-       P3 = P3_c[0] * pow(P3, 0) + P3_c[1] * pow(P3, 1); // in dPSI
-       P4 = P4_c[0] * pow(P4, 0) + P4_c[1] * pow(P4, 1); // in dPSI
-       P5 = P5_c[0] * pow(P5, 0) + P5_c[1] * pow(P5, 1); // in dPSI
-       r_ail = r_ail_c[0] * pow(r_ail, 0) + r_ail_c[1] * pow(r_ail, 1) + r_ail_c[2] * pow(r_ail, 2) + r_ail_c[3] * pow(r_ail, 3) + r_ail_c[4] * pow(r_ail, 4) + r_ail_c[5] * pow(r_ail, 5) + r_ail_c[6] * pow(r_ail, 6) + r_ail_c[7] * pow(r_ail, 7); // in degrees
-       l_ail = l_ail_c[0] * pow(l_ail, 0) + l_ail_c[1] * pow(l_ail, 1) + l_ail_c[2] * pow(l_ail, 2) + l_ail_c[3] * pow(l_ail, 3) + l_ail_c[4] * pow(l_ail, 4); // in degrees
-       r_ele = r_ele_c[0] * pow(r_ele, 0) + r_ele_c[1] * pow(r_ele, 1) + r_ele_c[2] * pow(r_ele, 2) + r_ele_c[3] * pow(r_ele, 3); // in degrees
-       l_ele = l_ele_c[0] * pow(l_ele, 0) + l_ele_c[1] * pow(l_ele, 1) + l_ele_c[2] * pow(l_ele, 2) + l_ele_c[3] * pow(l_ele, 3) + l_ele_c[4] * pow(l_ele, 4); // in degrees
-       rud = rud_c[0] * pow(rud, 0) + rud_c[1] * pow(rud, 1) + rud_c[2] * pow(rud, 2) + rud_c[3] * pow(rud, 3) + rud_c[4] * pow(rud, 4); // in degrees
 
        // Conversion of converted adc data to state data
-       P_avg = (P2 + P3 + P4 + P5) *0.25; // in dPSI
+       P_avg = (P2 + P3 + P4 + P5) * 0.25; // in dPSI
        C_alpha = (P4 - P5) / (P1 - P_avg); // unitless
        C_beta = (P3 - P2) / (P1 - P_avg); // unitless
-       alpha = 0.01745329251 * evaluate_poly(alpha_c, sizeof(alpha_c), C_alpha, C_beta); // in rad
-       beta = 0.01745329251 * evaluate_poly(beta_c, sizeof(beta_c), C_alpha, C_beta); // in rad
-       cps = evaluate_poly(cps_c, sizeof(cps_c), C_alpha, C_beta); // unitless
-       cpt = evaluate_poly(cpt_c, sizeof(cpt_c), C_alpha, C_beta); // unitless
+       alpha = 0.01745329251 * evaluate_poly(alpha_c, 15, C_alpha, C_beta); // in rad
+       beta = 0.01745329251 * evaluate_poly(beta_c, 15, C_alpha, C_beta); // in rad
+       cps = evaluate_poly(cps_c, 21, C_alpha, C_beta); // unitless
+       cpt = evaluate_poly(cpt_c, 15, C_alpha, C_beta); // unitless
        Pt = (P1 - cpt * (P1 - P_avg)) * 6894.76; // in Pa
        Ps = (P_avg - cps * (P1 - P_avg)) * 6894.76; // in Pa
-       rho = 1.1666; // in kg/m^3
-       V = sqrt((2.0 * (Pt - Ps)) / (rho)); // in m/s
+
+       // If the pressure readings indicate imaginary velocity, assing the velocity as the previous good value
+       if (Pt >= 0.0 || Ps >= 0.0 || Pt - Ps >= 0.0) {
+           std::cout << "Pressure readings rejected" << std::endl;
+           V = V_prev;
+       }
+       else {
+           V = sqrt((2.0 * (abs(Pt) - abs(Ps))) / (rho)); // in m/s
+       }
 
        // INS data
        yaw = 0.01745329251 * handlerObject.vnins.yaw; // in rad
@@ -300,12 +271,12 @@ int main(int argc, char *argv[])
        // Bad state rejection
        V = (V < V_min || V > V_max) ? V_prev : V;
        alpha = (alpha < alpha_min || alpha > alpha_max) ? alpha_prev : alpha;
-       q = (q < q_min || q > q_max) ? q_prev : q;
-       theta = (theta < theta_min || theta > theta_max) ? theta_prev : theta;
+       wy = (wy < q_min || wy > q_max) ? q_prev : wy;
+       pitch = (pitch < theta_min || pitch > theta_max) ? theta_prev : pitch;
        beta = (beta < beta_min || beta > beta_max) ? beta_prev : beta;
-       p = (p < p_min || p > p_max) ? p_prev : p;
-       r = (r < r_min || r > r_max) ? r_prev : r;
-       phi = (phi < phi_min || phi > phi_max) ? phi_prev : phi;
+       wx = (wx < p_min || wx > p_max) ? p_prev : wx;
+       wz = (wz < r_min || wz > r_max) ? r_prev : wz;
+       roll = (roll < phi_min || roll > phi_max) ? phi_prev : roll;
 
        // Calculate states
        lon_states[0] = V - V_0;
