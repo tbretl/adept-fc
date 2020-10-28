ebc31983190438f98c690ce7d474edb39b47cad4
GrayKS3248 <gschaer2@illinois.edu>
2020-10-28 14:14:07 -0500

    use wing_leveler AP as AP_main

--- src/autopilot_main.cpp
+++ src/autopilot_main.cpp
@@ -120,59 +120,49 @@ int main(int argc, char *argv[])
         double ps4_con[2] = { 4.8152e-06, -0.19283 }; // To dPSI
         double ps5_con[2] = { 4.8028e-06, -0.19231 }; // To dPSI
 
-        /*
-        // Conversion constants for ADC to control surface deflections
-        double rel_con[4] = { -1.8360e-12,  6.0969e-08, -0.00190000,  34.3486000 };
-        double lel_con[5] = { -4.1426e-16,  3.9725e-11, -1.4238e-06,  0.01960000, 68.407500 };
-        double ral_con[8] = {  4.8223e-28, -9.3369e-23,  7.6526e-18,  3.4398e-13, 9.1555e-9, -1.4428e-4, 1.2474000, 4588.2000 };
-        double lal_con[5] = { -1.7482e-16,  1.3728e-11, -4.0790e-07,  0.00450000, 4.0069000 };
-        double rud_con[5] = {  1.8988e-15, -2.1584e-10,  9.2592e-06, -0.17560000, 1212.7000 };
-         */
-
         // Conversion constants for deflection commands to PWM commands
-        double ail_PWM_con[6] = { -2.5025e-5,  7.2442e-4, 0.0042000, -0.11670, -19.7993,  1498.60 };
-        double ele_PWM_con[6] = { -1.0598e-6, -5.2453e-5, 7.6461e-5,  0.06800,  11.4908,  1528.60 };
-        double rud_PWM_con[7] = {  9.3548e-8, -6.1896e-6, 2.2718e-5,  0.00270,  0.00990, -12.6829, 1547.5 };
+        double ail_PWM_con[2] = { -19.9874, 1499.3311 };
+        double ele_PWM_con[3] = { 0.041213, 11.2938, 1529.0023 };
+        double rud_PWM_con[2] = { -12.3453, 1550.7985 };
 
         // Conversion constants for the converted adc data
-        double AoA_con[15] = {  0.14417, -14.0401, -0.48222, -0.82991, -0.39334, -0.06513,  0.29331,  0.10864,  0.57212,  0.12463,  0.022992,  0.02921,  0.08984,  0.057237,  0.016901 };
-        double bet_con[15] = { -0.04568,  0.09017,  13.8048,  0.00203,  0.94476,  0.29254,  0.12192, -0.66955, -0.02088, -0.33687,  0.023934, -0.10505, -0.01904, -0.054968, -0.018293 };
-        double cps_con[21] = {  0.59235, -0.00321, -0.00458, -0.09805,  0.01015, -0.09770, -0.01585, -0.00249, -0.02244, -0.00376, -0.004233, -0.00394,  0.01926, -0.001497, -0.0014977, -0.0007027, -0.0010546, 0.0041895, 6.6051e-05, 0.00048148, -0.00022731 };
-        double cpt_con[15] = { -0.21483,  0.03614,  0.00374, -0.12377, -0.03420, -0.11844,  0.00220,  0.00401,  0.00472,  0.00266,  0.000107,  0.00234,  0.00791,  0.003493, -0.001166 };
+        double AoA_con[15] = { 0.14417, -14.0401, -0.48222, -0.82991, -0.39334, -0.065129, 0.29331, 0.10864, 0.57212, 0.12463, 0.022992, 0.029209, 0.089836, 0.057237, 0.016901 };
+        double bet_con[15] = { -0.045676, 0.090171, 13.8048, 0.002027, 0.94476, 0.29254, 0.12192, -0.66955, -0.020875, -0.33687, 0.023934, -0.10505, -0.019041, -0.054968, -0.018293 };
+        double cps_con[21] = { 0.59235, -0.0032055, -0.0045759, -0.098045, 0.010147, -0.0977, -0.015851, -0.0024914, -0.022443, -0.0037574, -0.0042317, -0.0039405, 0.01926, -0.0014971, -0.0014967, -0.0007027, -0.0010546, 0.0041895, 6.6051e-05, 0.00048148, -0.00022731 };
+        double cpt_con[15] = { -0.21483, 0.036139, 0.0037369, -0.12377, -0.034201, -0.11844, 0.0022027, 0.0040131, 0.0047189, 0.0026645, 0.00010707, 0.0023433, 0.0079094, 0.0034925, -0.001166 };
 
-        // Wing Leveling Controller constants
-        double k_lon[2][4] = { {0.00000, 0.00000, 0.00000, 0.00000},  {0.00000, 0.00000, 0.00000, 0.00000} };// Longitudinal controller gains of form u_lon = -k_lon * x_lon
-        double k_lat[2][5] = { {0.00000, 0.14855, 0.00000, 0.40000, 0.00000},  {0.00000, 0.00000, 0.00000, 0.00000, 0.00000} };// Lateral controller gains of form u_lat = -k_lat * x_lat
+        // Controller constants
+        double k_lat[2][5] = { {0, 0.14855, 0, 0.4, 0}, {0, 0, 0, 0, 0} }; // Lateral controller gains of form u_lat = -k_lat * x_lat
 
         // Trim conditions
-        double vel_trm = 30.576; // m/s
+        double vel_trm = 30.5755; // m/s
         double AoA_trm = 0.0178; // rad
-        double wyy_trm = 0.0000; // rad/s
-        double pit_trm = 0.0000; // rad
-        double bet_trm = 0.0000; // rad
-        double wxx_trm = 0.0000; // rad/s
-        double wzz_trm = 0.0000; // rad/s
-        double rol_trm = 0.0000; // rad
-        double yaw_trm = 0.0000; // rad
+        double wyy_trm = 0; // rad/s
+        double pit_trm = 0; // rad
+        double bet_trm = 0; // rad
+        double wxx_trm = 0; // rad/s
+        double wzz_trm = 0; // rad/s
+        double rol_trm = 0; // rad
+        double yaw_trm = 0; // rad
         double rho_trm = 1.1666; // in kg/m^3
 
         // State limits
-        double vel_min =  0.0000; // Minimum acceptable value. Any values lower are considered improper readings.
-        double vel_max =  60.000; // Maximum acceptable value. Any values higher are considered improper readings.
+        double vel_min = 0; // Minimum acceptable value. Any values lower are considered improper readings.
+        double vel_max = 60; // Maximum acceptable value. Any values higher are considered improper readings.
         double AoA_min = -0.6108; // Minimum acceptable value. Any values lower are considered improper readings.
-        double AoA_max =  0.6108; // Maximum acceptable value. Any values higher are considered improper readings.
+        double AoA_max = 0.6108; // Maximum acceptable value. Any values higher are considered improper readings.
         double wyy_min = -0.6108; // Minimum acceptable value. Any values lower are considered improper readings.
-        double wyy_max =  0.6108; // Maximum acceptable value. Any values higher are considered improper readings.
+        double wyy_max = 0.6108; // Maximum acceptable value. Any values higher are considered improper readings.
         double pit_min = -1.0472; // Minimum acceptable value. Any values lower are considered improper readings.
-        double pit_max =  1.0472; // Maximum acceptable value. Any values higher are considered improper readings.
+        double pit_max = 1.0472; // Maximum acceptable value. Any values higher are considered improper readings.
         double bet_min = -0.6108; // Minimum acceptable value. Any values lower are considered improper readings.
-        double bet_max =  0.6108; // Maximum acceptable value. Any values higher are considered improper readings.
+        double bet_max = 0.6108; // Maximum acceptable value. Any values higher are considered improper readings.
         double wxx_min = -1.0471; // Minimum acceptable value. Any values lower are considered improper readings.
-        double wxx_max =  1.0471; // Maximum acceptable value. Any values higher are considered improper readings.
+        double wxx_max = 1.0471; // Maximum acceptable value. Any values higher are considered improper readings.
         double wzz_min = -0.5236; // Minimum acceptable value. Any values lower are considered improper readings.
-        double wzz_max =  0.5236; // Maximum acceptable value. Any values higher are considered improper readings.
+        double wzz_max = 0.5236; // Maximum acceptable value. Any values higher are considered improper readings.
         double rol_min = -1.0472; // Minimum acceptable value. Any values lower are considered improper readings.
-        double rol_max =  1.0472; // Maximum acceptable value. Any values higher are considered improper readings.
+        double rol_max = 1.0472; // Maximum acceptable value. Any values higher are considered improper readings.
 
         // Previous states
         double vel_pre = 0.0; // m/s
@@ -186,8 +176,8 @@ int main(int argc, char *argv[])
 
         // Input trim values
         double ele_trm = -0.0832; // rad
-        double ail_trm =  0.0000; // rad
-        double rud_trm =  0.0000; // rad
+        double ail_trm = 0; // rad
+        double rud_trm = 0; // rad
         double tr0_trm = 0.49188; //percent throttle [0.0, 1.0]
         double tr1_trm = 0.49188; //percent throttle [0.0, 1.0]
         double tr2_trm = 0.49188; //percent throttle [0.0, 1.0]
@@ -233,21 +223,9 @@ int main(int argc, char *argv[])
         double wxx;
         double wyy;
         double wzz;
-        double lon_sts[4];
         double lat_sts[5];
 
-        /*
-        // Declare all other control surface values used
-        double ele_lft;
-        double ele_rgt;
-        double ail_lft;
-        double ail_rgt;
-        double rud;
-        */
-
         // Declare all other input values used
-        double lon_in0;
-        double lon_in1;
         double lat_in0;
         double lat_in1;
         double ele_ang_cmd;
@@ -300,18 +278,10 @@ int main(int argc, char *argv[])
                 ps3 = evl_exp(ps3_con, 2, (double)handlerObject.adc.data[2] ); // uCH2
                 ps4 = evl_exp(ps4_con, 2, (double)handlerObject.adc.data[3] ); // uCH3
                 ps5 = evl_exp(ps5_con, 2, (double)handlerObject.adc.data[4] ); // uCH4
-                /*
-                ail_rgt = evl_exp(ral_con, 4, (double)handlerObject.adc.data[8] ); // dCH0
-                ail_lft = evl_exp(lal_con, 5, (double)handlerObject.adc.data[9] ); // dCH1
-                ele_rgt = evl_exp(rel_con, 8, (double)handlerObject.adc.data[10]); // dCH2
-                ele_lft = evl_exp(lel_con, 5, (double)handlerObject.adc.data[11]); // dCH3
-                rud     = evl_exp(rud_con, 5, (double)handlerObject.adc.data[12]); // dCH4
-                */
 
-                // Calculate pressure coefficient data
-                pes_avg = (ps2 + ps3 + ps4 + ps5)* 0.25;          // in dPSI
-                cof_AoA = (ps4 - ps5) / (ps1 - pes_avg);          // unitless
-                cof_bet = (ps3 - ps2) / (ps1 - pes_avg);          // unitless
+                pes_avg = (ps2 + ps3 + ps4 + ps5)* 0.25; // in dPSI
+                cof_AoA = (ps4 - ps5) / (ps1 - pes_avg); // unitless
+                cof_bet = (ps3 - ps2) / (ps1 - pes_avg); // unitless
                 cof_ptt = evl_ply(cpt_con, 15, cof_AoA, cof_bet); // unitless
                 cof_pst = evl_ply(cps_con, 21, cof_AoA, cof_bet); // unitless
 
@@ -360,41 +330,35 @@ int main(int argc, char *argv[])
                 rol_pre = rol; // rad
 
                 // Calculate -1.0 * (state errors)
-                lon_sts[0] = vel_trm - vel;
-                lon_sts[1] = AoA_trm - AoA;
-                lon_sts[2] = wyy_trm - wyy;
-                lon_sts[3] = pit_trm - pit;
                 lat_sts[0] = bet_trm - bet;
                 lat_sts[1] = wxx_trm - wxx;
                 lat_sts[2] = wzz_trm - wzz;
                 lat_sts[3] = rol_trm - rol;
                 lat_sts[4] = yaw_trm - yaw;
 
-                // Calculate lon inputs based on lon state (u-u0) = -K_lon * (x - x0)
-                lon_in0 = k_lon[0][0] * lon_sts[0] + k_lon[0][1] * lon_sts[1] + k_lon[0][2] * lon_sts[2] + k_lon[0][3] * lon_sts[3]; // u[0] - u[0]_0 for the lon sys
-                lon_in1 = k_lon[1][0] * lon_sts[0] + k_lon[1][1] * lon_sts[1] + k_lon[1][2] * lon_sts[2] + k_lon[1][3] * lon_sts[3]; // u[1] - u[1]_0 for the lon sys
-
-                // Calculate lat inputs based on lat state (u-u0) = -K_lat * (x - x0)
-                lat_in0 = k_lat[0][0] * lat_sts[0] + k_lat[0][1] * lat_sts[1] + k_lat[0][2] * lat_sts[2] + k_lat[0][3] * lat_sts[3] + k_lat[0][4] * lat_sts[4]; // u[0] - u[0]_0 for the lat sys
-                lat_in1 = k_lat[1][0] * lat_sts[0] + k_lat[1][1] * lat_sts[1] + k_lat[1][2] * lat_sts[2] + k_lat[1][3] * lat_sts[3] + k_lat[1][4] * lat_sts[4]; // u[1] - u[1]_0 for the lat sys
+                // Calculate input deltas based on state (u-u0) = -K * (x - x0)
+                lat_in0 = k_lat[0][0] * lat_sts[0] + k_lat[0][1] * lat_sts[1] + k_lat[0][2] * lat_sts[2] + k_lat[0][3] * lat_sts[3] + k_lat[0][4] * lat_sts[4]; // u[0] - u_0[0] for the lat subsystem
+                lat_in1 = k_lat[1][0] * lat_sts[0] + k_lat[1][1] * lat_sts[1] + k_lat[1][2] * lat_sts[2] + k_lat[1][3] * lat_sts[3] + k_lat[1][4] * lat_sts[4]; // u[1] - u_0[1] for the lat subsystem
 
                 // Convert angle commands to degrees
-                ele_ang_cmd = 57.29578*(lon_in0 + ele_trm); // in degrees
-                ail_ang_cmd = 57.29578*(lat_in0 + ail_trm); // in degrees
-                rud_ang_cmd = 57.29578*(lat_in1 + rud_trm); // in degrees
+                ele_ang_cmd = 57.29578 * ele_trm; // in degrees
+                ail_ang_cmd = 57.29578 * (lat_in0 + ail_trm); // in degrees
+                rud_ang_cmd = 57.29578 * (lat_in1 + rud_trm); // in degrees
 
-                // Convert angular degree commands to PWM commands
-                ele_PWM_cmd = (int) evl_exp(ele_PWM_con, 6, ele_ang_cmd);
-                ail_PWM_cmd = (int) evl_exp(ail_PWM_con, 6, ail_ang_cmd);
-                rud_PWM_cmd = (int) evl_exp(rud_PWM_con, 7, rud_ang_cmd);
-                tr0_PWM_cmd = (int) thr_PWM_min + (thr_PWM_max - thr_PWM_min) * (lon_in1 + tr0_trm);
-                tr1_PWM_cmd = (int) thr_PWM_min + (thr_PWM_max - thr_PWM_min) * (lon_in1 + tr1_trm);
-                tr2_PWM_cmd = (int) thr_PWM_min + (thr_PWM_max - thr_PWM_min) * (lon_in1 + tr2_trm);
-                tr3_PWM_cmd = (int) thr_PWM_min + (thr_PWM_max - thr_PWM_min) * (lon_in1 + tr3_trm);
-                tr4_PWM_cmd = (int) thr_PWM_min + (thr_PWM_max - thr_PWM_min) * (lon_in1 + tr4_trm);
-                tr5_PWM_cmd = (int) thr_PWM_min + (thr_PWM_max - thr_PWM_min) * (lon_in1 + tr5_trm);
-                tr6_PWM_cmd = (int) thr_PWM_min + (thr_PWM_max - thr_PWM_min) * (lon_in1 + tr6_trm);
-                tr7_PWM_cmd = (int) thr_PWM_min + (thr_PWM_max - thr_PWM_min) * (lon_in1 + tr7_trm);
+                // Convert angular deflection commands to PWM commands
+                ele_PWM_cmd = (int) evl_exp(ele_PWM_con, 3, ele_ang_cmd);
+                ail_PWM_cmd = (int) evl_exp(ail_PWM_con, 2, ail_ang_cmd);
+                rud_PWM_cmd = (int) evl_exp(rud_PWM_con, 2, rud_ang_cmd);
+
+                // Convert throttle commands to PWM commands
+                tr0_PWM_cmd = (int) thr_PWM_min + (thr_PWM_max - thr_PWM_min) * tr0_trm;
+                tr1_PWM_cmd = (int) thr_PWM_min + (thr_PWM_max - thr_PWM_min) * tr1_trm;
+                tr2_PWM_cmd = (int) thr_PWM_min + (thr_PWM_max - thr_PWM_min) * tr2_trm;
+                tr3_PWM_cmd = (int) thr_PWM_min + (thr_PWM_max - thr_PWM_min) * tr3_trm;
+                tr4_PWM_cmd = (int) thr_PWM_min + (thr_PWM_max - thr_PWM_min) * tr4_trm;
+                tr5_PWM_cmd = (int) thr_PWM_min + (thr_PWM_max - thr_PWM_min) * tr5_trm;
+                tr6_PWM_cmd = (int) thr_PWM_min + (thr_PWM_max - thr_PWM_min) * tr6_trm;
+                tr7_PWM_cmd = (int) thr_PWM_min + (thr_PWM_max - thr_PWM_min) * tr7_trm;
 
                 // Ensure PWM commands are within safe limits
                 ele_PWM_cmd = ele_PWM_cmd > ele_PWM_max ? ele_PWM_max : ele_PWM_cmd;
@@ -443,16 +407,12 @@ int main(int argc, char *argv[])
                         std::cout.precision(1);
                         std::cout<< "Ele CMD = " << ele_ang_cmd << " deg   |   ";
                         std::cout<< "Ele PWM = " << ele_PWM_cmd << "   |   ";
-
                         std::cout<< "Ail CMD = " << ail_ang_cmd << " deg   |   ";
                         std::cout<< "Ail PWM = " << ail_PWM_cmd << "   |   ";
-
                         std::cout<< "Rud CMD = " << rud_ang_cmd << " deg   |   ";
                         std::cout<< "Rud PWM = " << rud_PWM_cmd << "   |   ";
-
-                        std::cout<< "Thr CMD = " << (lon_in1 + tr0_trm)*100.0 << " %   |   ";
+                        std::cout<< "Thr CMD = " << tr0_trm*100.0 << " %   |   ";
                         std::cout<< "Thr PWM = " << tr0_PWM_cmd;
-
                         std::cout<< "\t\r" << std::flush;
                         */
                         cur_itr = 1;
